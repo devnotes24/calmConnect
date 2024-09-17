f649613d20e04cfa9b137febe71a05d4ecfe7185
Sachinvb <sachinvb27@gmail.com>
2024-09-17 13:22:48 +0530

    first commit

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,5 +3,4 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/regLoginRt', require('./regLoginRt'));
-router.use('/productDataRt', require('./productDataRt'));
 module.exports = router;
